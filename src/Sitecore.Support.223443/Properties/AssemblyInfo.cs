caa8bd3e49036d3bc64abb942c260427c4abab6a
ArtemNizelnyk <artem.nizelnik@gmail.com>
2019-03-05 16:37:50 +0800

    Init-Commit

---
+++ src/Sitecore.Support.223443/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.223443")]
+[assembly: AssemblyProduct("Sitecore.Support.223443")]
+[assembly: ComVisible(false)]
